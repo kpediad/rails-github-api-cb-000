a4759487045026731e0cbbca12d92f3e10e336fb
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-13 11:51:36 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,19 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    response = Faraday.get "https://api.github.com/user" do |req|
+      req.body = { 'client_id': client_id, 'client_secret': client_secret }
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = session[:token]
+    end
+    body = JSON.parse(response.body)
+    @username = body['login']
+    response = Faraday.get "https://api.github.com/user/repos" do |req|
+      req.body = { 'client_id': client_id, 'client_secret': client_secret }
+      req.headers['Accept'] = 'application/json'
+      req.headers['Authorization'] = session[:token]
+    end
+    @repos = JSON.parse(response.body)
   end
 
 end
